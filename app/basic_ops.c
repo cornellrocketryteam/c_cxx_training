82d3b1000ed1e785f9fa59f501f4aab74009044f
Aidan <acm289@cornell.edu>
2022-12-16 11:23:56 -0500

    Chapter 1 Practice

---
+++ app/basic_ops.c
@@ -0,0 +1,33 @@
+//----------------------------------------------------
+// Chapter 1 Practice: Basic Variables and Operations
+//----------------------------------------------------
+
+#include <stdio.h>
+
+int main( void )
+{
+    //====================================================
+    // PRACTICE TASK:
+    //
+    // Declare and define three variables of type int
+    // named test1, test2, and test3
+    //
+    // Your goal is to:
+    //  - Have test1 hold the value 17
+    //  - Have test2 hold the value 39
+    //  - Have test3 hold the value -14
+    //  - Use at least 7 operators (besides the assignment
+    //    operator) to achieve this
+    //
+    // The lines below will print out the values of these
+    // variables when you run the program, so you can
+    // check that you got it right
+    //
+    // (make sure to declare and define them above these
+    // lines)
+    //====================================================
+
+    printf("test1 holds the value %d\n", test1);
+    printf("test2 holds the value %d\n", test2);
+    printf("test3 holds the value %d\n", test3);
+}
